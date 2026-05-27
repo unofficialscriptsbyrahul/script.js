2d3324794dec61d7b4fde261c853333e620eed78
unofficialscriptsbyrahul <unofficialrahulsarkar@gmail.com>
2026-05-27 14:28:17 -0700

    Create bot.user.js

---
+++ core/ui/ui/automation/automation/automation/userscript/bot.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name Wallet Automation
+// @match *://*/*
+// @grant none
+// ==/UserScript==
+
+(async function(){
+
+  console.log(
+    "Wallet Automation Loaded"
+  );
+
+})();
